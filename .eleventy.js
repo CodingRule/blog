63a68d4bb3ec6e5243ef76d896ec12b15e0fc592
BasToTheMax <info@bastothemax.nl>
2024-03-25 18:07:21 +0100

    Better code highlight

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 };
