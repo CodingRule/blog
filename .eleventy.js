18ed86413b5e337d84af4b327fe13b60b8d83f23
CodingRule <91148727+CodingRule@users.noreply.github.com>
2024-03-30 15:19:12 +0200

    Delete .eleventy.js

--- .eleventy.js
+++
@@ -1,7 +0,0 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
-module.exports = function(eleventyConfig) {
-	eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-};
